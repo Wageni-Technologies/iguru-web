29a8da79c87a3d142e50a2836c616b747efa4d0b
Emmanuel Columbus Mugenyi <emmanuel.mugenyi@cellulant.io>
2024-04-07 18:19:16 +0300

    add analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,6 +16,7 @@ export default function RootLayout({ children }) {
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest"></link>
+        <meta name="google-site-verification" content="BQhlIoHEdi9Jla7JWTlKbQG2R5F1le8aXCvu8kudInU" />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
